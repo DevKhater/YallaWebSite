acc4361b25d3c3a599f0488de685a85fb2c541e4
DevKhater <m.f.khater@gmail.com>
2016-05-11 23:45:27 +0200

    Venue frontend page index

--- src/YallaWebsite/FrontendBundle/Controller/DefaultController.php
+++ src/YallaWebsite/FrontendBundle/Controller/DefaultController.php
@@ -3,16 +3,39 @@
 namespace YallaWebsite\FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use YallaWebsite\BackendBundle\Entity\Venue;
 
 class DefaultController extends Controller
 {
+
     public function indexAction()
     {
         return $this->render('YallaWebsiteFrontendBundle:Default:index.html.twig');
     }
-    
+
     public function aboutAction()
     {
         return $this->render('YallaWebsiteFrontendBundle:Page:about.html.twig');
     }
+
+    public function venuesAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $entities = $em->getRepository('YallaWebsiteBackendBundle:Venue');
+        $query = $entities->findAll();
+        dump($query);
+        if ($query != NULL) {
+            $paginator = $this->get('knp_paginator');
+            $pagination = $paginator->paginate(
+                    $query, $this->get('request')->query->get('page', 1), 1
+            );
+        } else {
+            $pagination = NULL;
+        }
+
+        return $this->render('YallaWebsiteFrontendBundle:Venue:index.html.twig',array(
+                    'pagination' => $pagination
+        ));
+    }
+
 }
