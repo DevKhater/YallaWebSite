149bdd6b127504b0b07e33e0257dede6369f5acc
DevKhater <m.f.khater@gmail.com>
2016-05-19 16:34:48 +0300

    Done Ajax Calendar and working

--- src/YallaWebsite/FrontendBundle/Controller/AjaxController.php
+++ src/YallaWebsite/FrontendBundle/Controller/AjaxController.php
@@ -9,16 +9,15 @@ class AjaxController extends Controller
 
     public function getEventByDateAction(Request $request)
     {
+        
         $theJSdate = substr($request->get('id'), 0, strpos($request->get('id'), '('));
         $date = date("Y-m-d", strtotime($theJSdate));
         $theDay = $date . '%';
         $em = $this->getDoctrine()->getManager();
         $entities = $em->getRepository('YallaWebsiteBackendBundle:Event');
         $allEvents = $entities->findEventsByDates($theDay);
-        $template = $this->forward('YallawebsiteFrontendBundle:Template:eventsAjax.html.twig', array('entities' => $allEvents))->getContent();
-        var_dump($template);exit;
+        $template = $this->render('YallaWebsiteFrontendBundle:Template:eventsAjax.html.twig', array('entities' => $allEvents))->getContent();
         $json = json_encode($template);
-        var_dump($json);
         $response = new Response($json, 200);
         $response->headers->set('Content-Type', 'application/json');
         return $response;
